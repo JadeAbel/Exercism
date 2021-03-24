6fb5624b8a77fb9904f74440749abce616625284
Jade Abel <jade.marie.abel@gmail.com>
2021-03-23 20:31:57 -0400

    Completed Resistor Color Duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,7 +2,22 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const COLOR = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (inputs) => {
+  //let inputs = input[0].toLowerCase();
+  return parseInt(
+    `${COLOR.indexOf(inputs[0]) + "" + COLOR.indexOf(inputs[1])}`
+  );
 };
